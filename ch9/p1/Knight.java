807d1bfb50e4c3c87825383d8d44147975dc2f0c
Алексей Шишкин <86236873+Shish-ok@users.noreply.github.com>
2022-05-06 17:11:29 +0300

    Create Knight.java

---
+++ ch9/p1/Knight.java
@@ -0,0 +1,47 @@
+package comm.netcracker.homework3.ch9.p1;
+
+import java.util.ArrayList;
+
+public class Knight implements ChessPiece {
+    private int row, column;
+    private String coordinates;
+    private final String color;
+
+    private final static int[][] steps = {
+            { -2, -1 },
+            { -2, 1 },
+            { -1, -2 },
+            { -1, 2 },
+            { 1, -2 },
+            { 1, 2 },
+            { 2, -1},
+            { 2, 1},
+    };
+
+    public Knight(String coordinates, String color) {
+        this.coordinates = coordinates;
+        this.color = color;
+        column = coordinates.charAt(0) - 97;
+        row = coordinates.charAt(1) - 48;
+    }
+
+    public void setPosition(String coordinates) {
+        this.coordinates = coordinates;
+        column = coordinates.charAt(0) - 97;
+        row = coordinates.charAt(1) - 48;
+    }
+
+    public String getPosition() { return coordinates; }
+
+    public ArrayList<String> canMoveTo() {
+
+        ArrayList<String> moves = new ArrayList<>();
+        for (int i = 0; i < 8; ++i) {
+            if (0 < row + steps[i][0] && row + steps[i][0] < 9 && -1 < column + steps[i][1] && column + steps[i][1] < 8) {
+                moves.add(Character.toString(column + steps[i][1] + 97) + Character.toString(row + steps[i][0] + 48));
+            }
+        }
+
+        return moves;
+    }
+}
